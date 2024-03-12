d334ce98c66bf0ddd40e61e6eea1fc78e1f090ce
Andriamahay11master <hirimanana@yahoo.fr>
2024-03-12 18:09:56 +0300

    add socle css/font

---
+++ taskmanager/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="appBlock">
+      <p>This is a test</p> <i className="icon-fb"></i>
+    </div>
+  );
+}
+
+export default App;
