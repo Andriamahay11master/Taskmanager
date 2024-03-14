e637f74710ea57a525c90b141e3fa33b9ed92a36
Andriamahay11master <hirimanana@yahoo.fr>
2024-03-14 17:04:16 +0300

    add component sign up

--- taskmanager/src/App.tsx
+++ taskmanager/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.scss';
 import Onboarding from './components/onboarding/Onboarding';
+import Signup from './components/signup/Signup';
 
 function App() {
   const dataOnboarding = {
@@ -12,9 +13,27 @@ function App() {
     labelButton: 'Get Started',
     routeButton: '/onboarding'
   }
+
+  const saveAccount = () => {
+    console.log('saveAccount');
+  }
+
+  const dataSIgnup = {
+    title: 'Create an account',
+    subtitle: 'Sign up',
+    username: 'Your Username',
+    email: 'Your Email',
+    password: 'Password',
+    labelButton: 'Sign up',
+    routeSignin: '/',
+    textUser: 'Already a user?',
+    labelSignin: 'Sign in',
+    saveAccount: saveAccount
+  }
   return (
     <div className="appBlock">
-      <Onboarding {...dataOnboarding}/>
+      {/* <Onboarding {...dataOnboarding}/> */}
+      <Signup {...dataSIgnup}/> 
     </div>
   );
 }
---
+++ taskmanager/src/components/signup/Signup.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+interface SignupProps {
+    title: string
+    subtitle?: string
+    username: string
+    email: string
+    password: string
+    labelButton: string
+    routeSignin: string
+    textUser: string
+    labelSignin: string
+    saveAccount: () => void
+}
+export default function Signup({title, subtitle, username, email, password, labelButton, routeSignin, textUser, labelSignin, saveAccount} : SignupProps) {
+    return (
+        <div className="form-block">
+            <h1 className="title-h1">{title}</h1>
+            <div className="form-content">
+                <h2 className="title-h2">{subtitle}</h2>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="username">{username}</label>
+                        <input type="text" id="username" placeholder="Write your username"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">{email}</label>
+                        <input type="email" id="email" placeholder="Write your email"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">{password}</label>
+                        <input type="password" id="password" placeholder="Write your password"/>
+                    </div>
+                    <div className="form-group form-submit">
+                        <button className="btn btn-primary" onClick={() => saveAccount()}>{labelButton}</button>
+                    </div>
+                </form>
+                <p>{textUser} <a className="btn btn-link" href={routeSignin}>{labelSignin}</a></p>
+            </div>
+        </div>
+    );
+}
