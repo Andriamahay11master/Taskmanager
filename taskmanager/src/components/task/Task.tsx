be8972a2d0ec7561f3553ba0c66921b7e046fb60
Andriamahay11master <hirimanana@yahoo.fr>
2024-03-19 14:26:07 +0300

    add component item task

--- taskmanager/src/components/task/Task.tsx
+++ taskmanager/src/components/task/Task.tsx
@@ -1,12 +1,19 @@
 
 import Menu from '../menu/Menu';
-import {dataMenu} from '../../data';
+import {dataMenu, listTasks} from '../../data';
 import './task.scss';
+import Itemtask from '../ItemTask/Itemtask';
 
 export default function Task() {
     return (
         
         <div className="appBlock">
+            <div className="appItem">
+                <h2 className="title-h2">All tasks</h2>
+                <div className="listTask">
+                    {listTasks.map((item, index) => <Itemtask key={index} task={item.task} time={item.time} />)}
+                </div>
+            </div>
             <Menu {...dataMenu}/>
         </div>
     )
