15adc43e9ed73f4ccf0f471a720e3f86721c3084
Andriamahay11master <hirimanana@yahoo.fr>
2024-04-04 16:59:18 +0300

    init add Category

--- src/components/parameter/Parameter.tsx
+++ src/components/parameter/Parameter.tsx
@@ -1,12 +1,89 @@
 
 import Menu from '../menu/Menu';
-import {dataMenu} from '../../data';
+import {dataMenu, listIconFeather} from '../../data';
 import './parameter.scss';
+import { Poppin } from '../poppin/Poppin';
+import { useState } from 'react';
+import { addDoc, collection, getDocs, limit, orderBy, query } from 'firebase/firestore';
+import { db } from '../../firebase';
+import { useEffect } from 'react';
 
 export default function Parameter() {
+    const [success, setSuccess] = useState(false);
+    const [labelCategory, setLabelCategory] = useState('');
+    const [iconCategory, setIconCategory] = useState('');
+    const [color, setColor] = useState('');
+    const [idCategory, setIdCategory] = useState<number | null>(null);
+
+    const resetValForm = () => {
+        setLabelCategory('');
+        setIconCategory('');
+        setColor('');
+    }
+
+    const fetchLastId = async () => {
+        try {
+            const q = query(collection(db, "category"), orderBy("id", "desc"), limit(1)); // Limit to 1 document
+            const querySnapshot = await getDocs(q);
+            if (!querySnapshot.empty) {
+                const lastId = querySnapshot.docs[0].data().id;
+                setIdCategory(lastId + 1); // Set the new ID as the last ID + 1
+            } else {
+                setIdCategory(1); // If no documents found, set ID to 1
+            }
+        } catch (error) {
+            console.error("Error fetching last ID: ", error);
+        }
+    }
+
+    const addCategory = async () => {
+        try{
+            if(labelCategory && iconCategory) {
+                await addDoc(collection(db, "category"), {
+                    id: idCategory,
+                    label: labelCategory,
+                    icon: iconCategory,
+                    color: color,
+                });
+                setSuccess(true);
+                resetValForm();
+            }  
+        }
+        catch (error) {
+            console.error("Error adding document: ", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchLastId();
+    })
     return (
         
         <div className="appBlock">
+            <div className="appForm">
+                <div className="form-group">
+                    <label htmlFor="task">Add Category</label>
+                    <input className="input-forms" type="text" id="labelCategory" placeholder="Add category" value={labelCategory} onChange={(e) => setLabelCategory(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="category">Icons category</label>
+                    <select className="input-forms" name="category" id="category" value={iconCategory} onChange={(e) => setIconCategory(e.target.value)}>
+                        {listIconFeather.map((icon) => (
+                            <option key={icon} value={icon}>
+                                <i className={icon}></i>{icon}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="task">Color category</label>
+                    <input className="input-forms" type="color" id="color" placeholder="Add color" value={color} onChange={(e) => setColor(e.target.value)}/>
+                </div>
+                <div className="form-group form-submit">
+                    <button className="btn btn-primary" onClick={addCategory}><i className="icon-check"></i></button>
+                </div>
+            </div>
+            {success && <Poppin title="Success" message="Category added successfully" linkBtn='/home' valBtn='Validate'/>}
             <Menu {...dataMenu}/>
         </div>
     )
